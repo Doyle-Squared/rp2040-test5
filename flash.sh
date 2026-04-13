74d3b0f7bc96024f84b71bc8d741a93e06d37828
Doyle-Squared <adoyle28@asu.edu>
2026-04-12 23:01:01 -0700

    Commented out WSL bash script, before I forget, so others can test the build system properly

--- flash.sh
+++ flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Shell script by Aidan Doyle 
+#Shell script by Aidan Doyle, updates to come
 
 BIN_FILE="./rp2040-freertos.uf2"
 
@@ -22,20 +22,20 @@ if [ ! -f "$BIN_FILE" ]; then
 fi
 
 echo "Flashing..."
-# sudo picotool load -x rp2040-freertos.uf2 -f
+sudo picotool load -x rp2040-freertos.uf2 -f
 
 #FOR WSL USERS ONLY. UNCOMMENT CODE BELOW IF USING WSL ON WINDOWS
 #Automates flashing if using WSL and running both powershell scripts for Pico BOOTSEL and Standard mode. 
 #Device Addresses are for Pico WH, not the regular Pico
-if  lsusb -d "2e8a:000a" > /dev/null ; then
-    sudo picotool reboot -f -u
-fi
+# if  lsusb -d "2e8a:000a" > /dev/null ; then
+#     sudo picotool reboot -f -u
+# fi
 
-until lsusb -d 2e8a:0003; do                        #Standard Mode VID:PID 
-echo "Waiting for Pico in BOOTSEL mode..."
-sleep 1
-done
-echo "Device Found!"
-sleep 1
+# until lsusb -d 2e8a:0003; do                        #Standard Mode VID:PID 
+# echo "Waiting for Pico in BOOTSEL mode..."
+# sleep 1
+# done
+# echo "Device Found!"
+# sleep 1
              
-sudo picotool load -x rp2040-freertos.uf2           #BOOTSEL Mode VID:PID
+# sudo picotool load -x rp2040-freertos.uf2           #BOOTSEL Mode VID:PID
