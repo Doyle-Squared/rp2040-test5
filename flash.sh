5d495dd77d00af11110ffc98f33ec1998dea0d1d
Doyle-Squared <adoyle28@asu.edu>
2026-04-13 22:31:17 -0700

    Added colors to bash shell script to highlight when the flash command is about to be run, when the binary cant be found, and when waiting for the pico if using WSL

--- flash.sh
+++ flash.sh
@@ -2,6 +2,12 @@
 
 #Shell script by Aidan Doyle, updates to come
 
+GREEN=$(tput setaf 2)
+CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+
 BIN_FILE="./rp2040-freertos.uf2"
 
 if [ ! -d "build/" ]; then
@@ -17,11 +23,11 @@ make
 cd src/
 
 if [ ! -f "$BIN_FILE" ]; then
-  echo "Firmware binary not found at $BIN_FILE"
+  echo "${RED}Firmware binary not found at $BIN_FILE${RESET}"
   exit 1
 fi
 
-echo "Flashing..."
+echo "${CYAN}Flashing...${RESET}"
 # picotool load -x rp2040-freertos.uf2 -f
 
 #FOR WSL USERS ONLY. UNCOMMENT CODE BELOW IF USING WSL ON WINDOWS
@@ -33,10 +39,10 @@ if  lsusb -d "2e8a:000a" > /dev/null ; then
 fi
 
 until lsusb -d 2e8a:0003; do                        #Standard Mode VID:PID 
-echo "Waiting for Pico in BOOTSEL mode..."
+echo "${YELLOW}Waiting for Pico in BOOTSEL mode...${RESET}"
 sleep 1
 done
 
-echo "Device Found!"
+echo "${GREEN}Device Found!${RESET}"
              
 picotool load -x rp2040-freertos.uf2           #BOOTSEL Mode VID:PID
